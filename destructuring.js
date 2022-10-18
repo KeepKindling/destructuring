77a1bd808bf196def8cd9486fbbe5b39c66bdbf6
KeepKindling <flashywizard127@gmail.com>
2022-10-18 19:57:52 +0000

    Initial commit

--- destructuring.js
+++ destructuring.js
@@ -4,9 +4,26 @@
  */
 
 // Destructuring arrays
+let ages = [30, 26, 27]
+// let jhon = ages[0];
+// let mary = ages[1];
+// let joe = ages[2];
+let [jhon, mary, joe] = ages;
+
+console.log(jhon, mary, joe);
 
 // Destructuring objects
+let jobs = {
+    mike: "designer",
+    jill: "developer",
+    alicia: "accountant"
+};
+let {mike, jill, alicia} = jobs;
+console.log(mike, jill, alicia)
 
 // Destructuring subsets
+let languages = ["English", "French", "Spanish", "German", "Japanese"];
+let [jhonNative, jhonSecondary] = languages;
+
 
 // Using rest parameter syntax
